5f057bf262c7d95441842475e727d1949964d504
i311648 <asya.chakarova@sap.com>
2018-10-25 18:08:16 +0300

    premium container

--- src/bg/fmi/mjt/lab/coffee_machine/container/PremiumContainer.java
+++ src/bg/fmi/mjt/lab/coffee_machine/container/PremiumContainer.java
@@ -1,33 +1,47 @@
 package bg.fmi.mjt.lab.coffee_machine.container;
 
+import bg.fmi.mjt.lab.coffee_machine.supplies.Beverage;
+
 public class PremiumContainer extends Container{
+    
+    private static final double PREMIUM_CONTAINER_INITIAL_WATER = 1000;
+    private static final double PREMIUM_CONTAINER_INITIAL_COFFEE = 1000;
+    private static final double PREMIUM_CONTAINER_INITIAL_MILK = 1000;
+    private static final double PREMIUM_CONTAINER_INITIAL_CACAO = 300;
+    double water = PREMIUM_CONTAINER_INITIAL_WATER;
+    double coffee = PREMIUM_CONTAINER_INITIAL_COFFEE;
+    double milk = PREMIUM_CONTAINER_INITIAL_MILK;
+    double cacao = PREMIUM_CONTAINER_INITIAL_CACAO;
 
     public PremiumContainer() {
-        // TODO Auto-generated constructor stub
     }
 
     @Override
-    public double getCurrentWater() {
-        // TODO Auto-generated method stub
-        return 0;
+    public double getCurrentWater() {   
+        return this.water;
     }
 
     @Override
     public double getCurrentMilk() {
-        // TODO Auto-generated method stub
-        return 0;
+        return this.milk;
     }
 
     @Override
     public double getCurrentCoffee() {
-        // TODO Auto-generated method stub
-        return 0;
+        return this.coffee;
     }
 
     @Override
     public double getCurrentCacao() {
-        // TODO Auto-generated method stub
-        return 0;
+        return this.cacao;
+    }
+
+    public void useSupplies(Beverage beverage) {
+        this.water = this.water - beverage.getWater();
+        this.coffee = this.coffee - beverage.getCoffee();
+        this.milk = this.milk - beverage.getMilk();
+        this.cacao = this.cacao - beverage.getCacao();
+        
     }
 
 }
